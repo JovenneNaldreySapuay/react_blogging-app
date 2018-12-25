30cb684c3cf53595928a84cf207e09d02ba8dba9
jovenne <jovenneregs@gmail.com>
2018-12-25 16:53:54 +0800

    update server modules and package.json

--- server/index.js
+++ server/index.js
@@ -1,31 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-
-mongoose.set('useFindAndModify', false);
-
-// schema here
-
-dotenv.config();
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true});
-mongoose.set('useCreateIndex', true);
+import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); 
-
-// routes here
-
-mongoose.connection.once('open', () => {
-	console.log("Successfully connected to Mongodb...");
-}).on('error', error => {
-	console.log("Connection failed:", error);
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Running on localhost:${PORT}`));
 process.setMaxListeners(0);
